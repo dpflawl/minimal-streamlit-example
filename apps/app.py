5c8025c2c8eaf6d6e4da1c3a208eb2bc1223716c
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-05-30 19:32:20 +0900

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
+from webcam import webcam
 import cv2
 
 import torch
@@ -8,16 +7,12 @@ import torch
 st.title("얼굴로부터 심박수를 추정하는 심박 모니터링 서비스 Demo 📷💓")
 st.write("👩‍💻 Developed by 지예림") 
 
-class VideoProcessor:
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-
-webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
+captured_image = webcam()
+if captured_image is None:
+    st.write("Waiting for capture...")
+else:
+    st.write("Got an image from the webcam:")
+    st.image(captured_image)
         
 #loaded_model = joblib.load("/app/minimal-streamlit-example/apps/PhysNet_v5.pkl")
 loaded_model = torch.load('/app/minimal-streamlit-example/apps/model_90.pt', encoding='ascii')
