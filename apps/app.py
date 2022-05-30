78f594225adfc80cd5d7f353a8fcfe41b1a036e5
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-05-30 19:00:47 +0900

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -5,7 +5,7 @@ import torch
 st.title("얼굴로부터 심박수를 추정하는 심박 모니터링 서비스 Demo 📷💓")
 st.write("👩‍💻 Developed by 지예림") 
 
-webrtc_streamer(key="example")
+webrtc_streamer(key="loopback")
         
 #loaded_model = joblib.load("/app/minimal-streamlit-example/apps/PhysNet_v5.pkl")
 loaded_model = torch.load('/app/minimal-streamlit-example/apps/model_90.pt', encoding='ascii')
