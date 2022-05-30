5b01d4119904c663bf996d5370beceac085c32a7
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-05-30 14:38:32 +0900

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("얼굴로부터 심박수를 추정하는 심박 모니터링 서비스 Demo 📷💓")
 st.write("👩‍💻 개발자: 지예림") 
-run = st.checkbox('심박 모니터링 시작')
+run = st.checkbox('모니터링 시작')
 FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
 
@@ -12,7 +12,7 @@ while run:
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
 else:
-    st.write('모니터링 지')
+    st.write('모니터링 중지')
 
 #머신러닝으로 저장된 모델을 호출하고 st로 부터 받은 값으로 예측한다.
 #loaded_model = joblib.load("/app/minimal-streamlit-example/apps/regression_model.pkl")
