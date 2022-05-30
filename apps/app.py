a42d666c6ba9c56323b7562f93421978278179e7
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-05-30 18:24:51 +0900

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -1,28 +1,11 @@
 import streamlit as st
-import cv2
+from streamlit_webrtc import webrtc_streamer
 import torch
 
 st.title("얼굴로부터 심박수를 추정하는 심박 모니터링 서비스 Demo 📷💓")
 st.write("👩‍💻 Developed by 지예림") 
-run = st.checkbox('모니터링 시작')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
 
-count = 0
-st.write(camera.isOpened())
-if camera.isOpened():
-    while run:
-        _, frame = camera.read()
-        if frame is not None: 
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            FRAME_WINDOW.image(frame)
-        else:
-            break
-        count += 1
-        if count == 91:
-            break
-    else:
-        st.write(' ') #st.write('모니터링 중지')
+webrtc_streamer(key="example")
         
 #loaded_model = joblib.load("/app/minimal-streamlit-example/apps/PhysNet_v5.pkl")
 loaded_model = torch.load('/app/minimal-streamlit-example/apps/model_90.pt', encoding='ascii')
