2a884f11cf3af4ff5aef601de50fe30ca3c53d8a
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-05-30 20:44:47 +0900

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -1,18 +1,16 @@
 import streamlit as st
-from webcam import webcam
+#from webcam import webcam
+#import cv2
+from streamlit_webrtc import webrtc_streamer
 import cv2
+import av #strealing video library
 
 import torch
 
 st.title("얼굴로부터 심박수를 추정하는 심박 모니터링 서비스 Demo 📷💓")
 st.write("👩‍💻 Developed by 지예림") 
 
-captured_image = webcam()
-if captured_image is None:
-    st.write("Waiting for capture...")
-else:
-    st.write("Got an image from the webcam:")
-    st.image(captured_image)
+webrtc_streamer(key='example')
         
 #loaded_model = joblib.load("/app/minimal-streamlit-example/apps/PhysNet_v5.pkl")
 loaded_model = torch.load('/app/minimal-streamlit-example/apps/model_90.pt', encoding='ascii')
