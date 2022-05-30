529dbe0cc5b7ac7b73c08777e723af211501fe20
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-05-30 13:39:45 +0900

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -1,25 +1,17 @@
-
-#streamlit 라이브러리를 불러오기
+import cv2
 import streamlit as st
-#AI모델을 불러오기 위한 joblib 불러오기
-import joblib
-import pandas as pd
-import os
 
-# st를 이용하여 타이틀과 입력 방법을 명시한다.
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
 
-st.title('차량 속도에 따른 납부보험료 예측서비스')
-st.subheader('(1) 아래에 당신 차의 평균 속도를 입력해주세요!!!')
-
-values = st.slider('평균속도 입력하세요', 0, 200)  # st.number_input("평균속도")
-st.write('평균속도:', values)
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
 
 #머신러닝으로 저장된 모델을 호출하고 st로 부터 받은 값으로 예측한다.
 loaded_model = joblib.load("/app/minimal-streamlit-example/apps/regression_model.pkl")
-new_x = [values]
-df_new_x = pd.DataFrame(new_x)
-new_y = loaded_model.predict(df_new_x )
-
-#예측결과를 화면에 뿌려준다. 
-st.subheader('(2) 속도에 따른 당신이 납부해야할 보험료는 다음과 같습니다.')
-st.write('예상되는 납부보험료:', new_y[0])
