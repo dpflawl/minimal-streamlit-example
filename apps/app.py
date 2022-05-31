6abef6fa47f0ea0124e8c4ac70cfa437925bf0fd
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-05-31 21:31:01 +0900

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -6,26 +6,11 @@ import time
 st.title("얼굴로부터 심박수를 추정하는 심박 모니터링 서비스 Demo 📷💓")
 st.write("👩‍💻 Developed by 지예림") 
 
-@st.cache(allow_output_mutation=True)
-def get_cap():
-    return cv.VideoCapture(0)
-
-cap = get_cap()
-
-frameST = st.empty()
-param=st.sidebar.slider('chose your value')
-
-while True:
-    ret, frame = cap.read()
-    # Stop the program if reached end of video
-    if not ret:
-        print("Done processing !!!")
-        cv.waitKey(3000)
-        # Release device
-        cap.release()
-        break
-
-    frameST.image(frame, channels="BGR")
+for i in range(-50, 50):
+    cap = cv2.VideoCapture(i)
+    is_camera = cap.isOpened()
+    if is_camera:
+        st.write(f"Input {i} is a valid camera value for VIDEO_SOURCE")
 
 #img_file = st.camera_input()
 #if img_file is not None:
