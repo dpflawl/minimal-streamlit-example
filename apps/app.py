d60f425d97c33e4a2d6cf061b7e52f2221d4fe69
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-05-31 21:27:51 +0900

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -6,39 +6,26 @@ import time
 st.title("얼굴로부터 심박수를 추정하는 심박 모니터링 서비스 Demo 📷💓")
 st.write("👩‍💻 Developed by 지예림") 
 
-cap = cv2.VideoCapture(0)
-frameST = st.empty()
-param = st.sidebar.slider('choose your value')
+@st.cache(allow_output_mutation=True)
+def get_cap():
+    return cv.VideoCapture(0)
 
-stopper_started = False
+cap = get_cap()
+
+frameST = st.empty()
+param=st.sidebar.slider('chose your value')
+
 while True:
-    success, frame = cap.read()
-    if not success: break
+    ret, frame = cap.read()
+    # Stop the program if reached end of video
+    if not ret:
+        print("Done processing !!!")
+        cv.waitKey(3000)
+        # Release device
+        cap.release()
+        break
 
     frameST.image(frame, channels="BGR")
-    updated_time = time.time()
-
-    if not stopper_started:
-        #this block executes for once with every
-        #streamlit re-run command
-        def stopper(self):
-            while True:
-                try:
-                    #if time difference increases this thread will be terminated
-                    time_diff = round(time.time() - updated_time)
-                    if time_diff >1: #1 second
-                        print("Done processing !!!")
-                        print("Releasing VideoCapture")
-                        #if streamlit ui is stopped
-                        #time gap will increase
-                        #hence releasing camera resource
-                        cap.release()
-                        break
-                except: pass
-        th = Thread(target=stopper, args=(0,))
-        th.daemon = True
-        th.start()
-        stopper_started = True
 
 #img_file = st.camera_input()
 #if img_file is not None:
