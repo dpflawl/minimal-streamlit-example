f3a156349379c3e3741669051f823b97d66e4996
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-05-31 21:06:56 +0900

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -10,9 +10,16 @@ import torch
 st.title("얼굴로부터 심박수를 추정하는 심박 모니터링 서비스 Demo 📷💓")
 st.write("👩‍💻 Developed by 지예림") 
 
-img_file = st.camera_input("Webcam image")
-if img_file is not None:
-    st.image(img_file)
+for i in range(0, 5):
+    cap = cv2.VideoCapture(i)
+    is_camera = cap.isOpened()
+    if is_camera:
+        st.write(f"Input {i} is a valid camera value for VIDEO_SOURCE")
+        cap.release()
+
+#img_file = st.camera_input()
+#if img_file is not None:
+#    st.image(img_file)
 
 #loaded_model = joblib.load("/app/minimal-streamlit-example/apps/PhysNet_v5.pkl")
 loaded_model = torch.load('/app/minimal-streamlit-example/apps/model_90.pt', encoding='ascii')
